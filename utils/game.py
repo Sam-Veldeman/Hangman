41f804bd3505f150cd95295a8221579d149212b7
Sam Veldeman <sam.veldeman@gmail.com>
2023-06-15 14:31:57 +0200

    Forgot to check for integer/float input

--- utils/game.py
+++ utils/game.py
@@ -1,27 +1,42 @@
 #Project Hangman
 #This project will start the well known 'Hangman' game.
-#Start the game with: 
+#Start the game with: python main.py (in ur terminal when inside the folder)
 #You get 5 lives to guess the word.
+
 #Create class hangman
 import random
+def check_user_input(input):
+    try:
+        # Convert it into integer
+        val = int(input)
+        return True
+    except ValueError:
+        try:
+            # Convert it into float
+            val = float(input)
+            return True
+        except ValueError:
+            return False
 class Hangman:
     #declare all the attributes.
     def __init__(self):
         self.possible_words = {1:'becode',2:'learning',3:'mathematics',4:'sessions'}
-        self.word_to_find = self.possible_words[random.randint(1,len(self.possible_words))].upper()
+        self.word_to_find = self.possible_words[random.randint(1,len(self.possible_words))].upper() #list comprehension with randomize (import)
         self.lives = 5
-        self.correctly_guessed_letters = ["_" for x in self.word_to_find]
+        self.correctly_guessed_letters = ["_" for x in self.word_to_find] #list comprehension to create list with '_' for letters in word to find
         self.wrongly_guessed_letters = []
         self.turn_count = 1
         self.error_count = 0
-    #recieve input from user, restrainted to 1 letter only.
-    def play(self,letter):
-        while True:
-            letter = input(f"Turn {self.turn_count}\nLooking for: {self.correctly_guessed_letters}\n\nPlease enter your letter:").upper()
+    #recieve input from user, restrainted to 1 letter input and check for previous use of a letter.
+    def play(self,letter): #declare method
+        while True: #check input untill input = ok
+            letter = input(f"Turn {self.turn_count}\n{self.lives} lives left\nLooking for: {self.correctly_guessed_letters}\n\nPlease enter your letter:").upper()
             if len(letter) != 1:
                 print("Please make sure to enter only 1 letter, please try again:")
             elif letter in self.wrongly_guessed_letters:
                 print("This letter was allready used. Please try again:")
+            elif check_user_input(letter) == True:
+                print("Only letters allowed! Try again.")
             else:
                 break
         if letter in self.word_to_find:
